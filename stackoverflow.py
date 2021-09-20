ce03a10f5aef2a6ada9edbff65bec13a60937cd7
JungSW <jungsunwoong01@gmail.com>
2021-09-21 01:05:39 +0900

    StackOverflow extract job

--- stackoverflow.py
+++ stackoverflow.py
@@ -11,6 +11,12 @@ def get_last_page():
     last_page = pages[-2].get_text(strip = True)
     return int(last_page)
 
+def extract_job(html):
+    title = html.find("h2").find("a")["title"]
+    company, location = html.find("h3").find_all("span", recursivd=False)
+    print(company.get_text(strip=True), location.get_text(strip=True))
+    return {'title':title}
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
@@ -18,7 +24,9 @@ def extract_jobs(last_page):
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class":"-job"})
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 def get_jobs():
     last_page = get_last_page()
