d4a7210df71dfcb3b14e3ff2dff22a59bf5f9372
JungSW <jungsunwoong01@gmail.com>
2021-09-20 22:49:31 +0900

    StackOverflow Pages

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class":"pagination"})
@@ -35,7 +35,7 @@ def extract_job(html):
         'link': f"https://www.indeed.com/viewjob?jk={job_id}"
     }
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -45,4 +45,9 @@ def extract_indeed_jobs(last_page):
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
